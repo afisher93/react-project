70c1df94843dcf06d622c5d5fa2840ea0596b47a
afisher93 <106286620+afisher93@users.noreply.github.com>
2023-06-06 20:31:19 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import FilmsList from "./components/filmsList";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: ["ready", "set", "go"],
+      text: "",
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    let newList = [...this.state.text, this.state.text];
+    this.setState({ list: newList, text: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            value={this.state.text}
+            onChange={(event) => this.setState({ text: event.target.value })}
+          />
+          <button type="submit">add</button>
+        </form>
+        <ul>
+          {this.state.list.map((item, idx) => {
+            return <li key={item + idx}>{item}</li>;
+          })}
+        </ul>
+        <FilmsList />
+      </div>
+    );
+  }
+}
